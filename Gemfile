fba022ddabd14df824ffb09d465a82caa088f98d
Harukao <mk2@mk2.red>
2016-07-02 18:51:34 +0900

    諸々追加

--- Gemfile
+++ Gemfile
@@ -6,4 +6,4 @@ source "https://rubygems.org"
 gem 'opal',           :github => 'opal/opal'
 gem 'opal-browser',   :github => 'opal/opal-browser'
 gem 'opal-haml',      :github => 'opal/opal-haml'
-gem 'hyalite',        :github => 'youchan/hyalite'
+gem 'hyalite',        :github => 'mk2/hyalite'
--- app/application.rb
+++ app/application.rb
@@ -7,11 +7,24 @@ class TodoView
   def initial_state
     @last_id = 0
     @todos = [{id: @last_id, completed: false, content: "何かの予定"}]
-    {todos: @todos}
+    {todos: @todos, show_completed: false}
   end
 
-  def on_change_task(e)
-    p e.target.value
+  def on_change_task(e, id)
+    @todos = @todos.map do |todo|
+      if todo[:id] == id
+        todo[:content] = e.target.value
+      end
+      todo
+    end
+    set_state(todos: @todos)
+  end
+
+  def on_click_delete_task_button(e, id)
+    @todos = @todos.select do |todo|
+      todo[:id] != id
+    end
+    set_state(todos: @todos)
   end
 
   def on_click_add_task_button(e)
@@ -21,40 +34,67 @@ class TodoView
     set_state(todos: @todos)
   end
 
+  def on_click_complete_task_button(e, id)
+    @todos = @todos.map do |todo|
+      if todo[:id] == id
+        todo[:completed] = true
+      end
+      todo
+    end
+    set_state(todos: @todos)
+  end
+
+  def on_toggle_show_completed_switch(e)
+    puts "on_toggle_show_completed_switch"
+    set_state(show_completed: !@state[:show_completed])
+  end
+
   def component_did_mount
   end
 
   def render
+    p @state
+
     todo_elements = @state[:todos].map do |todo|
-      Hyalite.create_element("tr", nil,
-        Hyalite.create_element("td", nil,
-          Hyalite.create_element("div", {className: "input-group"},
-            Hyalite.create_element("button", {className: "btn btn-primary input-group-btn"}, "-"),
-            Hyalite.create_element("input", {className: "form-input", placeholder: "タスク", value: todo[:content], onChange: -> (event) { on_change_task(event) } }),
-            Hyalite.create_element("button", {className: "btn btn-primary input-group-btn"}, "Fin")
+      !@state[:show_completed] && todo[:completed] ? nil :
+        Hyalite.create_element("tr", {key: "tr-#{todo[:id]}"},
+          Hyalite.create_element("td", nil,
+            Hyalite.create_element("div", {className: "input-group"},
+              Hyalite.create_element("button", {className: "btn btn-primary input-group-btn", onClick: -> (event) { on_click_delete_task_button(event, todo[:id]) }}, "-"),
+              todo[:completed] ?
+                Hyalite.create_element("input", {className: "form-input", placeholder: "タスク", value: todo[:content], readOnly: "true", disabled: "true"}) :
+                Hyalite.create_element("input", {className: "form-input", placeholder: "タスク", value: todo[:content], onChange: -> (event) {on_change_task(event, todo[:id])} }),
+              Hyalite.create_element("button", {className: "btn btn-primary input-group-btn", onClick: -> (event) { on_click_complete_task_button(event, todo[:id]) }}, "Fin")
+              )
             )
           )
-        )
     end
 
-    p todo_elements
-
-    add_task_button =
-      Hyalite.create_element("tr", nil,
-        Hyalite.create_element("td", {colspan: 3},
-          Hyalite.create_element("button", {className: "btn btn-block", onClick: -> (event) { on_click_add_task_button(event) } }, "+")
+    completed_tasks_switch =
+      Hyalite.create_element("div", {className: "form-group"},
+        Hyalite.create_element("label", {className: "form-switch"},
+          @state[:show_completed] ?
+            Hyalite.create_element("input", {type: "checkbox", checked: "checked", onChange: -> (event) {on_toggle_show_completed_switch(event)} }) :
+            Hyalite.create_element("input", {type: "checkbox", onChange: -> (event) {on_toggle_show_completed_switch(event)} }),
+          Hyalite.create_element("i", {className: "form-icon" }),
+          "完了済みも表示する"
           )
         )
+
+    add_task_button =
+      Hyalite.create_element("button", {className: "btn btn-block", onClick: -> (event) { on_click_add_task_button(event) } }, "+")
 
     Hyalite.create_element("div", {className: 'container'},
       Hyalite.create_element("div", {className: 'columns'},
         Hyalite.create_element("div", {className: 'column col-3'}),
         Hyalite.create_element("div", {className: 'column col-6'},
+          completed_tasks_switch,
+          add_task_button,
           Hyalite.create_element("table", {className: "table table-striped table-hover"},
-            add_task_button,
             todo_elements
             )
           ),
+
         Hyalite.create_element("div", {className: 'column col-3'})
         )
       )
